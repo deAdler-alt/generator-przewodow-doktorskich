ed2aad6ea03ea1a84e0c6aaf1a7135c15ea80e01
Kamil Piejko <173197@stud.prz.edu.pl>
2025-05-31 10:21:50 +0200

    D5: login/logout i ochrona widoków

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -7,4 +8,7 @@ urlpatterns = [
     path('doktoraty/<int:pk>/', views.DoktoratDetailView.as_view(), name='doktorat_detail'),
     path('doktoraty/<int:pk>/edytuj/', views.DoktoratUpdateView.as_view(), name='doktorat_update'),
     path('doktoraty/<int:pk>/usun/', views.DoktoratDeleteView.as_view(), name='doktorat_delete'),
+# Logowanie i wylogowanie
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,27 +1,35 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Doktorat
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
+
+@method_decorator(login_required, name='dispatch')
 class DoktoratListView(ListView):
     model = Doktorat
     template_name = 'core/doktorat_list.html'
 
+@method_decorator(login_required, name='dispatch')
 class DoktoratDetailView(DetailView):
     model = Doktorat
     template_name = 'core/doktorat_detail.html'
 
+@method_decorator(login_required, name='dispatch')
 class DoktoratCreateView(CreateView):
     model = Doktorat
     fields = ['doktorant', 'tytul', 'promotor', 'promotorPomocniczy', 'kopromotor', 'rozprawa', 'komentarz']
     template_name = 'core/doktorat_form.html'
     success_url = reverse_lazy('doktorat_list')
 
+@method_decorator(login_required, name='dispatch')
 class DoktoratUpdateView(UpdateView):
     model = Doktorat
     fields = ['doktorant', 'tytul', 'promotor', 'promotorPomocniczy', 'kopromotor', 'rozprawa', 'komentarz']
     template_name = 'core/doktorat_form.html'
     success_url = reverse_lazy('doktorat_list')
 
+@method_decorator(login_required, name='dispatch')
 class DoktoratDeleteView(DeleteView):
     model = Doktorat
     template_name = 'core/doktorat_confirm_delete.html'
